6b85b2ccc041b346b6c0a90ad8d4d0a51e173f76
Mieszko Kuzmierz <mieszko.kuzmierz@gmail.com>
2021-07-22 21:57:27 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const name = 'Mieszko';
+const age = 35;
+
+alert('Witam na mojej stronie!');
+console.log('WTF000');
+console.log('SIEMAAA');
+console.log(name);
+console.log(age);
+console.log(`Siema, jestem ${name} i mam ${age} lat`)
